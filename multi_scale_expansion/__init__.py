19da4db79fc34ecdc7bbac00d1f0ced541c96fca
Angel Mancera <mancera.angel@columbia.edu>
2023-03-26 23:14:19 -0400

    Bump version: 0.1.0 → 0.1.1

--- multi_scale_expansion/__init__.py
+++ multi_scale_expansion/__init__.py
@@ -2,4 +2,4 @@ from .classification import train_model, save_learning_curve
 from .model import get_plant_model, get_train_loss_needs
 from .dataset import PlantDataset, collate_fn, get_datasets, get_dataloaders
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
